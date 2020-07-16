d13407f03195c20041c29a57b3943c323d08a444
Sigmo4ka <sigm@ukr.net>
2020-07-16 16:51:10 +0300

    beautified files

--- wp-content/themes/vestibulum/header.php
+++ wp-content/themes/vestibulum/header.php
@@ -3,8 +3,7 @@
 <head>
 	<meta http-equiv="Content-type" content="text/html; charset=<?php bloginfo('charset'); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-	<title><?php wp_title('«', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-	<!-- <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" /> -->
+	<title><?php wp_title('«', true, 'right'); ?> <?php bloginfo('name'); ?></title>	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<?php wp_head(); ?>
 </head>
